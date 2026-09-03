f0f3659b7f1c62f56a40bdddb10b226664ceb39e
Mohamed Khadra <m.khadra@penta-b.com>
2026-09-03 19:51:43 +0300

    feat(server): add pmgisdb database to the aspire apphost

--- server/PMGIS.AppHost/AppHost.cs
+++ server/PMGIS.AppHost/AppHost.cs
@@ -1,3 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgres = builder.AddPostgres("postgres")
+  .WithImage("postgis/postgis")
+  .WithImageTag("16-3.4")
+  .WithDataVolume("pmgis-pgdata")
+  .WithLifetime(ContainerLifetime.Persistent);
+
+var database = postgres.AddDatabase("pmgisdb");
+
 builder.Build().Run();
