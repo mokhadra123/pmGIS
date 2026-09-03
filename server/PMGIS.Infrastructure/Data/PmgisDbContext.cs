b967d8f4603c6099c2a76f06ab7c681237af2561
Mohamed Khadra <m.khadra@penta-b.com>
2026-09-03 19:52:06 +0300

    feat(infrastructure): add data access project and db context

---
+++ server/PMGIS.Infrastructure/Data/PmgisDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using PMGIS.Domain.Entities;
+
+namespace PMGIS.Infrastructure.Data;
+
+public class PmgisDbContext(DbContextOptions<PmgisDbContext> options) : DbContext(options)
+{
+    public DbSet<Project> Projects => Set<Project>();
+    public DbSet<Activity> Activities => Set<Activity>();
+    public DbSet<ProjectType> ProjectTypes => Set<ProjectType>();
+    public DbSet<User> Users => Set<User>();
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.HasPostgresExtension("postgis");
+    modelBuilder.ApplyConfigurationsFromAssembly(typeof(PmgisDbContext).Assembly);
+
+    base.OnModelCreating(modelBuilder);
+  }
+}
