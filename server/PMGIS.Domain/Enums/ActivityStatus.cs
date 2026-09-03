2b4204634e676d7de15375696c6e61c49dd6878b
Mohamed Khadra <m.khadra@penta-b.com>
2026-09-03 19:09:14 +0300

    feat(domain): add project and activity status enums

---
+++ server/PMGIS.Domain/Enums/ActivityStatus.cs
@@ -0,0 +1,9 @@
+namespace PMGIS.Domain.Enums;
+
+public enum ActivityStatus
+{
+    Planned = 0,
+    InProgress = 1,
+    Completed = 2,
+    OnHold = 3,
+}
---
+++ server/PMGIS.Domain/Enums/ProjectStatus.cs
@@ -0,0 +1,10 @@
+namespace PMGIS.Domain.Enums;
+
+public enum ProjectStatus
+{
+    Draft = 0,
+    Active = 1,
+    InProgress = 2,
+    OnHold = 3,
+    Completed = 4,
+}
